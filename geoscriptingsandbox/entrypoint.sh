20598b6dbec58ea9d066894745efc76653256e5b
laiskaSiili <marc.folini@gmail.com>
2022-04-18 12:57:50 +0200

    Make it clear what files will be copied to geoscripting sandbox image. Add env variable whether home is being persisted and make file overwrite conditional on that.

--- geoscriptingsandbox/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Upon container start copy overwrite existing files on shared volume. Files which are
-# not in conflict will remain, giving user the chance to just copy and rename to persist changes.
-cp -rT /setup/tutorial_notebooks_img /home/tutorial_notebooks
-
-# Run the CMD as the main container process
-exec "$@"
