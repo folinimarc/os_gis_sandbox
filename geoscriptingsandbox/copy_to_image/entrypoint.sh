840e3840c2fb6d105b624d807bcd7332687b0643
laiskasiili <48335835+laiskasiili@users.noreply.github.com>
2022-04-30 20:21:57 +0200

    Rename datastories-container to generic content-container and move all content there. (#9)

--- geoscriptingsandbox/copy_to_image/entrypoint.sh
+++ geoscriptingsandbox/copy_to_image/entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 
-if $HOME_IS_PERSISTED
+if $PERSISTENCE_NOTE_IN_HOME
 then
-    # Upon container start copy overwrite existing files on shared volume. Files which are
-    # not in conflict will remain, giving user the chance to just copy and rename to persist changes.
-    cp -rT /assets/tutorial_notebooks_img /home/tutorial_notebooks
     # Copy readme explaining persistence and overwrite procedure.
     cp /assets/_README.md /home/_README.md
 fi
