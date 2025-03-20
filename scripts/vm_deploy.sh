bfc7270f0154b9daceff0570967953e759b4f9f0
laiskasiili <marc.folini@gmail.com>
2025-03-20 20:35:46 +0100

    Fix deploy script to interpolate hostname, username, password more reliably

--- scripts/vm_deploy.sh
+++ scripts/vm_deploy.sh
@@ -60,9 +60,9 @@ git clone -b test/reverse_proxy https://github.com/folinimarc/os_gis_sandbox.git
 cd os_gis_sandbox
 
 # Create .env file and replace crucial parameters
-sed -i "s/OSGS_HOSTNAME=localhost/OSGS_HOSTNAME=${HOSTNAME}/" sandbox.deploy.conf
-sed -i "s/OSGS_USERNAME=os_gis_sandbox/OSGS_USERNAME=${USERNAME}/" sandbox.conf
-sed -i "s/OSGS_PASSWORD=you_should_change_this/OSGS_PASSWORD=${PASSWORD}/" sandbox.conf
+sed -i "s/OSGS_HOSTNAME=[^ ]*/OSGS_HOSTNAME=${HOSTNAME}/" sandbox.deploy.conf
+sed -i "s/OSGS_USERNAME=[^ ]*/OSGS_USERNAME=${USERNAME}/" sandbox.conf
+sed -i "s/OSGS_PASSWORD=[^ ]*/OSGS_PASSWORD=${PASSWORD}/" sandbox.conf
 
 # Start Sandbox
 docker compose -f compose.yml -f compose.deploy.yml --env-file sandbox.conf --env-file sandbox.deploy.conf up -d
